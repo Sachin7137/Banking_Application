79cad07633188570be32bf11c065966982c747d5
Jarvis <sachinaajage7137@gmail.com>
2026-01-12 19:51:40 +0530

    Initial commit

---
+++ src/service/impl/BankServiceImpl.java
@@ -0,0 +1,145 @@
+package service.impl;
+
+import domain.Account;
+import domain.Customer;
+import domain.Transaction;
+import domain.Type;
+import exceptions.AccountNotFoundException;
+import exceptions.InsufficientFundsException;
+import exceptions.ValidationException;
+import repository.AccountRepository;
+import repository.CustomerRepository;
+import repository.TransactionRepository;
+import service.BankService;
+import util.Validation;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class BankServiceImpl implements BankService {
+    private AccountRepository accountRepository = new AccountRepository();
+    private TransactionRepository transactionRepository = new TransactionRepository();
+    private CustomerRepository customerRepository = new CustomerRepository();
+
+    private final Validation<String> validateName = name -> {
+        if(name == null || name.isBlank()) throw new ValidationException(" Validate Name is required");
+    };
+
+    private final Validation<String> validateEmail = email -> {
+      if(email == null || !email.contains("@")) throw new ValidationException("Validate Email is required");
+    };
+
+    private final Validation<String> validateType = type -> {
+        if(type == null || (type.contains("SAVINGS") || type.contains("CURRENT"))) throw new ValidationException("Validate type is required");
+    };
+
+    private final Validation<Double> validateAmountPositive = amount -> {
+        if(amount == null || amount < 0) throw new ValidationException("Amount must be positive");
+    };
+
+    @Override
+    public String openAccount(String name, String email, String accountType) {
+        validateName.validate(name);
+        validateEmail.validate(email);
+        validateType.validate(accountType);
+
+        String customerId = UUID.randomUUID().toString();
+
+//        Create Customer
+        Customer c = new Customer(customerId, name, email);
+        customerRepository.save(c);
+
+//        Change later
+//        String accountNumber = UUID.randomUUID().toString();
+        String accountNumber = getAccountNumber();
+        Account account = new Account(accountNumber, customerId, (double) 0, accountType);
+        accountRepository.save(account);
+        return accountNumber;
+    }
+
+    @Override
+    public List<Account> listAccounts() {
+        return accountRepository.findAll().stream()
+                .sorted(Comparator.comparing(Account::getAccountNumber))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deposit(String accountNumber, Double amount, String note) {
+        validateAmountPositive.validate(amount);
+        Account account = accountRepository.findByNumber(accountNumber).orElseThrow(() -> new AccountNotFoundException("Account not found: " + accountNumber));
+        account.setBalance(account.getBalance() + amount);
+        Transaction transaction = new Transaction(account.getAccountNumber(), amount, UUID.randomUUID().toString(), note, LocalDateTime.now(), Type.DEPOSIT);
+        transactionRepository.add(transaction);
+    }
+
+    @Override
+    public void withdraw(String accountNumber, Double amount, String note) {
+        validateAmountPositive.validate(amount);
+        Account account = accountRepository.findByNumber(accountNumber).orElseThrow(() -> new AccountNotFoundException("Account not found: " + accountNumber));
+        if (account.getBalance().compareTo(amount) < 0)
+            throw new InsufficientFundsException("Insufficient balance");
+        account.setBalance(account.getBalance() - amount);
+        Transaction transaction = new Transaction(account.getAccountNumber(), amount, UUID.randomUUID().toString(), note, LocalDateTime.now(), Type.WITHDRAW);
+        transactionRepository.add(transaction);
+    }
+
+    @Override
+    public void transfer(String fromAcc, String toACC, Double amount, String note) {
+        validateAmountPositive.validate(amount);
+        if (fromAcc.equals(toACC))
+            throw new ValidationException("Cannot transfer to your own account");
+        Account from = accountRepository.findByNumber(fromAcc)
+                .orElseThrow(() -> new AccountNotFoundException("Account not found: " + fromAcc));
+        Account to = accountRepository.findByNumber(toACC)
+                .orElseThrow(() -> new AccountNotFoundException("Account not found: " + toACC));
+        if (from.getBalance().compareTo(amount) < 0)
+            throw new InsufficientFundsException("Insufficient balance");
+        from.setBalance(from.getBalance() - amount);
+        to.setBalance(to.getBalance() + amount);
+
+//        Transaction fromTransaction = new Transaction(from.getAccountNumber(), amount, UUID.randomUUID().toString(), note, LocalDateTime.now(), Type.WITHDRAW);
+//        transactionRepository.add(fromTransaction);
+
+        transactionRepository.add( new Transaction(from.getAccountNumber(),
+                amount, UUID.randomUUID().toString(), note, LocalDateTime.now(), Type.TRANSFER_OUT));
+
+        transactionRepository.add( new Transaction(to.getAccountNumber(),
+                amount, UUID.randomUUID().toString(), note, LocalDateTime.now(), Type.TRANSFER_IN));
+
+    }
+
+    @Override
+    public List<Transaction> getStatement(String account) {
+        return transactionRepository.finByAccount(account).stream()
+                .sorted(Comparator.comparing(Transaction::getTimestamp))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Account> searchAccountByCustomerName(String q) {
+        String query = (q == null) ? "" : q.toLowerCase();
+//        List<Account> result = new ArrayList<>();
+//        for(Customer c : customerRepository.findAll()){
+//            if(c.getName().toLowerCase().contains(query))
+//                result.addAll(accountRepository.findByCustomerId(c.getId()));
+//        }
+//        result.sort(Comparator.comparing(Account::getAccountNumber));
+//        return result;
+
+//        Using Stream
+        return customerRepository.findAll().stream()
+                .filter(c -> c.getName().toLowerCase().contains(query))
+                .flatMap(c -> accountRepository.findByCustomerId(c.getId()).stream())
+                .sorted(Comparator.comparing(Account::getAccountNumber))
+                .collect(Collectors.toList());
+    }
+
+    private String getAccountNumber() {
+        int size = accountRepository.findAll().size() + 1;
+        return String.format("AC%06d", size);
+    }
+}
